f58a2e9ef5e19c5690cf029c93cde96fc985f57a
Lestomos <diegolestomos@yahoo.com>
2017-12-30 21:58:56 +0100

    e1 - e4 conflict?

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -2,15 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("e1");
 		System.out.println("e2");
 		System.out.println("e3");
-=======
-		
-		
-		
 		System.out.println("e4");
->>>>>>> branch 'master' of https://github.com/Lestomos/hellogit.git
+
 	}
 }
