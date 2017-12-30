4a3bd70ddb36b22d7b0b8c3de545ab74ddabeed1
Lestomos <diegolestomos@yahoo.com>
2017-12-30 22:00:42 +0100

    e4

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -2,9 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("e1");
-		System.out.println("e2");
-		System.out.println("e3");
 		System.out.println("e4");
 	}
 }
