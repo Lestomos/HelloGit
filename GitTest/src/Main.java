116c1a9b99ba1c6ff2e79f67dd2b5d9e459ee85e
Lestomos <diegolestomos@yahoo.com>
2017-12-30 23:13:32 +0100

    a1 from scratch

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -2,11 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("f1");
-	}
-	
-	public static void do1() {
-		System.out.println("do1 - c2");
+		System.out.println("a1");
 	}
 }
-//abc
