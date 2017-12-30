a0a26b431d42d7b9dd7736d3c2676ff22b173e67
Lestomos <diegolestomos@yahoo.com>
2017-12-30 22:24:29 +0100

    ttt

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -6,10 +6,6 @@ public class Main {
 	}
 	
 	public static void do1() {
-<<<<<<< HEAD
-		System.out.println("do1 - b2");
-=======
 		System.out.println("do1 - b1");
->>>>>>> branch 'master' of https://github.com/Lestomos/hellogit.git
 	}
 }
