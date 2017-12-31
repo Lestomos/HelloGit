3fa63ee6164d9113b63786e2056e74694278ec67
Lestomos <diegolestomos@yahoo.com>
2017-12-31 02:43:37 +0100

    b3

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("b2");
+		System.out.println("b3");
 	}
 	
 	public static void do1() {
-		System.out.println("nada");
+		System.out.println("nada de nada");
 	}
 }
